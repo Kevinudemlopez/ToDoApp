80f3dac6f2b8cb2f1054bfd429af96ef33fb6334
Kevinudemlopez <kaguirre070@soyudemedellin.edu.co>
2024-08-22 13:54:50 -0500

    Cree la clase TodoBook

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -11,4 +11,42 @@ class Todo:
         self.completed: False
         self.tags = []
 
-    
+    def mark_completed(self):
+        self.completed = True
+
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def __str__(self) -> str:
+        return f"{self.code_id} - {self.title}"
+
+
+class TodoBook:
+    def __init__(self):
+        self.todos = {}
+
+    def add_todo(self, title: str, description: str) -> int:
+        todo_id = len(self.todos) + 1
+        todo = Todo(todo_id, title, description)
+        self.todos[todo_id] = todo
+        return todo_id
+
+    def pending_todos(self) -> list[Todo]:
+        return [todo for todo in self.todos.values() if not todo.completed]
+
+    def completed_todos(self) -> list[Todo]:
+        return [todo for todo in self.todos.values() if todo.completed]
+
+    def tags_todo_count(self) -> dict[str, int]:
+        tag_count = {}
+        for todo in self.todos.values():
+            for tag in todo.tags:
+                if tag in tag_count:
+                    tag_count[tag] += 1
+                else:
+                    tag_count[tag] = 1
+        return tag_count
+
+
+    pass
