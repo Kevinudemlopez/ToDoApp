9bb0971bd4120ea4b075e531b7af33306a74f042
Kevinudemlopez <kaguirre070@soyudemedellin.edu.co>
2024-08-22 13:52:50 -0500

    Cree la clase todo

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,14 @@
-# TODO: Add code here
+
+
+
+class Todo:
+
+    def __init__(self, code_id: (int), title: (str), description: (str), completed: (bool), tags: (list[str])  ) -> None:
+
+        self.code_id: int
+        self.title: str
+        self.description: str
+        self.completed: False
+        self.tags = []
+
+    
